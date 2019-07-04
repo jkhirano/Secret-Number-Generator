86644170ff3b024877154d78d76c82fe3a913a2d
Janelle K Hirano <jaahnelle@gmail.com>
2019-07-03 17:57:33 -1000

    updated

--- secret_number.js
+++ secret_number.js
@@ -1,4 +1,14 @@
 'use strict';
 module.exports = function() {
-  
+
+let secretNumberGenerator = function() {
+    return Math.floor((Math.random()*1000000)+1)
+    }
+    // console.log(secretNumberGenerator());
+    let secretNumber = secretNumberGenerator();
+    let storedNumber = function() {
+        return secretNumber;
+    }
+    return storedNumber;
+
 };
