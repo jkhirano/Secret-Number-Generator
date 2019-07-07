3072e493d55d2b8e83afb4acc628ca4cdb14d89b
Janelle K Hirano <jaahnelle@gmail.com>
2019-07-07 13:18:11 -1000

    removed console

--- secret_number.js
+++ secret_number.js
@@ -1,14 +1,11 @@
-'use strict';
+"use strict";
 module.exports = function() {
-
-let secretNumberGenerator = function() {
-    return Math.floor((Math.random()*1000000)+1)
-    }
-    // console.log(secretNumberGenerator());
-    let secretNumber = secretNumberGenerator();
-    let storedNumber = function() {
-        return secretNumber;
-    }
-    return storedNumber;
-
+  let secretNumberGenerator = function() {
+    return Math.floor(Math.random() * 1000000 + 1);
+  };
+  let secretNumber = secretNumberGenerator();
+  let storedNumber = function() {
+    return secretNumber;
+  };
+  return storedNumber;
 };
